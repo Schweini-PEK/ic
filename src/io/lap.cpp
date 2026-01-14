2a5c2f4a07e57e008dcca97311afa60839f77e8f
Fengzhe Shi <fengzheshi@gmail.com>
2026-01-13 16:35:47 -0800

    bug fixed

--- src/io/lap.cpp
+++ src/io/lap.cpp
@@ -1,63 +1,55 @@
 #include "ichol/mtx_read.hpp"
 
-template <typename T>
-ichol::matrix::CsrMatrix<T> gen_laplacian_csr(int n)
+namespace ichol::io
 {
-    ichol::matrix::CsrMatrix<T> laplacian;
-    laplacian.num_rows = n * n;
-    laplacian.num_cols = n * n;
-    laplacian.nnz = 5 * n * n - 4 * n;
-    laplacian.row_ptr.resize(laplacian.num_rows + 1);
-    laplacian.col_ind.resize(laplacian.nnz);
-    laplacian.values.resize(laplacian.nnz);
-
-    int nnz_index = 0;
-    for (int i = 0; i < n; ++i)
+    template <typename T>
+    ichol::matrix::CsrMatrix<T> gen_laplacian_csr(int n)
     {
-        for (int j = 0; j < n; ++j)
+        ichol::matrix::CsrMatrix<T> laplacian;
+        laplacian.num_rows = n * n;
+        laplacian.num_cols = n * n;
+        // Store only lower triangle + diagonal to match solver expectations.
+        laplacian.nnz = 3 * n * n - 2 * n;
+        laplacian.row_ptr.resize(laplacian.num_rows + 1);
+        laplacian.col_ind.resize(laplacian.nnz);
+        laplacian.values.resize(laplacian.nnz);
+
+        int nnz_index = 0;
+        for (int i = 0; i < n; ++i)
         {
-            int row = i * n + j;
-            laplacian.row_ptr[row] = nnz_index;
-
-            // Diagonal element
-            laplacian.col_ind[nnz_index] = row;
-            laplacian.values[nnz_index] = 4;
-            nnz_index++;
-
-            // Left neighbor
-            if (j > 0)
+            for (int j = 0; j < n; ++j)
             {
-                laplacian.col_ind[nnz_index] = row - 1;
-                laplacian.values[nnz_index] = -1;
-                nnz_index++;
-            }
+                int row = i * n + j;
+                laplacian.row_ptr[row] = nnz_index;
 
-            // Right neighbor
-            if (j < n - 1)
-            {
-                laplacian.col_ind[nnz_index] = row + 1;
-                laplacian.values[nnz_index] = -1;
-                nnz_index++;
-            }
+                // Left neighbor (lower triangle)
+                if (j > 0)
+                {
+                    laplacian.col_ind[nnz_index] = row - 1;
+                    laplacian.values[nnz_index] = -1;
+                    nnz_index++;
+                }
 
-            // Top neighbor
-            if (i > 0)
-            {
-                laplacian.col_ind[nnz_index] = row - n;
-                laplacian.values[nnz_index] = -1;
-                nnz_index++;
-            }
+                // Top neighbor (lower triangle)
+                if (i > 0)
+                {
+                    laplacian.col_ind[nnz_index] = row - n;
+                    laplacian.values[nnz_index] = -1;
+                    nnz_index++;
+                }
 
-            // Bottom neighbor
-            if (i < n - 1)
-            {
-                laplacian.col_ind[nnz_index] = row + n;
-                laplacian.values[nnz_index] = -1;
+                // Diagonal element (must be last in row)
+                laplacian.col_ind[nnz_index] = row;
+                laplacian.values[nnz_index] = 4;
                 nnz_index++;
             }
         }
-    }
-    laplacian.row_ptr[laplacian.num_rows] = nnz_index;
+        laplacian.row_ptr[laplacian.num_rows] = nnz_index;
 
-    return laplacian;
-}
+        return laplacian;
+    }
+
+    template ichol::matrix::CsrMatrix<double> gen_laplacian_csr(int n);
+    template ichol::matrix::CsrMatrix<float> gen_laplacian_csr(int n);
+    template ichol::matrix::CsrMatrix<half_float::half> gen_laplacian_csr(int n);
+} // namespace ichol::io
